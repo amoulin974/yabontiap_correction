071cdddc69979859c3e3490d1e726ab5d0c2bb25
Antoine Moulin <amoulin006@iutbayonne.univ-pau.fr>
2024-08-19 10:23:48 +0200

    version 1.2 ajout twig recettes

--- recettes.php
+++ recettes.php
@@ -1,104 +1,70 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    <!-- <link rel='stylesheet' type='text/css' href='style.css'> -->
-    <title>Yabon ! - Les recettes</title>
-</head>
-<body class="container">
-    
-
-    
-    <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Yabontiap v1</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="index.php">Catégories</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active" href="recettes.php">Recettes</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="recettes_tableau.php">Recette_tableau</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="categories_tableau.php">Catégories tableau</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
-    </header>
 
 
-        <main>
-            <h1>Yabon !</h1>
-            <?php
-            //Récupération de la catégorie de recette dans le Get
-            //ATTENTION Cette méthode de travail n'est pas sécurisée. LA bonne méthode sera abordée ultérieurement
+<?php
 
-            $idCategorie = isset($_GET['id_categorie']) ? $_GET['id_categorie'] : null;
+//ajout de l’autoload de composer
+require_once 'vendor/autoload.php';
 
+//ajout de la classe IntlExtension et creation de l’alias IntlExtension
+use Twig\Extra\Intl\IntlExtension;
 
-            //Connexion à la base de données en pdo
-            $pdo = new PDO('mysql:host=localhost;dbname=yabontiap_bd', 'root', '');
+//initialisation twig : chargement du dossier contenant les templates
+$loader = new Twig\Loader\FilesystemLoader('templates');
 
-            //Construction de la requête
-            $sql = "SELECT * FROM yabontiap_recette R";
-            if (isset($idCategorie)) {
-                $sql .= " WHERE R.id_categorie=:id_categorie";
-            }
+//Paramétrage de l'environnement twig
+$twig = new Twig\Environment($loader, [
+    /*passe en mode debug à enlever en environnement de prod : permet d'utiliser dans un templates {{dump
+    (variable)}} pour afficher le contenu d'une variable. Nécessite l'utilisation de l'extension debug*/
+    'debug' => true,
+    // Il est possible de définir d'autre variable d'environnement
+    //...
+]);
 
+//Définition de la timezone pour que les filtres date tiennent compte du fuseau horaire français.
+$twig->getExtension(\Twig\Extension\CoreExtension::class)->setTimezone('Europe/Paris');
 
-            $pdoStatement = $pdo->prepare($sql);
+//Ajouter l'extension debug
+$twig->addExtension(new \Twig\Extension\DebugExtension());
 
-            if (isset($idCategorie)) {
-                $pdoStatement->execute(array(':id_categorie' => $idCategorie));
-            }else{
-                $pdoStatement->execute();
-            }
-
-            $recettes = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
-            ?>
-            <div id="zone_cartes" class="row row-cols-3">
-
-                <?php
-                foreach ($recettes as $recette) {
-                    ?>
-
-                    <!--Les cartes-->
-                    <a href="recette.php?id_recette=<?= $recette['id'] ?>" class="col mb-3">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= "image/" . $recette['image'] ?>" class="card-img-top" alt="">
-                            <div class="card-body bg-primary">
-                                <h5><?= $recette['nom'] ?></h5>
-                            </div>
-                        </div>
-                    </a>                
-
-                <?php } ?>
-
-            </div>
-            
-        </main>
-
-        <footer class="text-body-secondary py-5">
-            <p class="mb-1">Ce site est un contre exemple. Il montre ce qu'il ne faut pas faire </p>
-            <p><a href="licence.php">Les licences des images</a></p>
-    </footer>
+//Ajout de l'extension d'internationalisation qui permet d'utiliser les filtres de date dans twig
+$twig->addExtension(new IntlExtension());
 
 
-    </body>
-</html>
+//Récupération de la catégorie de recette dans le Get
+//ATTENTION Cette méthode de travail n'est pas sécurisée. LA bonne méthode sera abordée ultérieurement
+
+$idCategorie = isset($_GET['id_categorie']) ? $_GET['id_categorie'] : null;
+
+
+//Connexion à la base de données en pdo
+$pdo = new PDO('mysql:host=localhost;dbname=yabontiap_bd', 'root', '');
+
+//Construction de la requête
+$sql = "SELECT * FROM yabontiap_recette R";
+if (isset($idCategorie)) {
+    $sql .= " WHERE R.id_categorie=:id_categorie";
+}
+
+
+$pdoStatement = $pdo->prepare($sql);
+
+if (isset($idCategorie)) {
+    $pdoStatement->execute(array(':id_categorie' => $idCategorie));
+}else{
+    $pdoStatement->execute();
+}
+
+$recettes = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+
+
+$template = $twig->load('recettes.html.twig');
+           
+echo $template->render(array(
+    'recettes' => $recettes 
+
+));
+           
+       
 
 
 
