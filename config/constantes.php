7a91148d96a3fa7bcaad840d4706b8c53e042f7d
Antoine Moulin <amoulin006@iutbayonne.univ-pau.fr>
2024-08-20 10:39:56 +0200

    ajout base_template sur index

---
+++ config/constantes.php
@@ -0,0 +1,16 @@
+<?php
+
+//Définir les constantes pour la configuration de la base de données
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'yabontiap_bd');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('PREFIXE_TABLE', 'yabontiap_');
+
+
+//Définir les constantes pour la configuration de l'application
+define('WEBSITE_TITLE', 'Yabontiap');
+define('WEBSITE_VERSION', '2.5.0');
+define('WEBSITE_LANGUAGE', 'fr');
+define('WEBSITE_DESCRIPTION', 'Un site de recettes de cuisine');
---
+++ include.php
@@ -0,0 +1,13 @@
+<?php
+//ajout de l’autoload de composer
+require_once 'vendor/autoload.php';
+
+
+//Chargement de la page des constantes
+require_once 'config/constantes.php';
+
+//Chargement de la connexion à la base de données en pdo
+require_once 'config/connexion.php';
+
+//initialisation de twig
+require_once 'config/twig.php';
