ab13eae332b3d084dd961d43435dde93252c5219
Antoine Moulin <amoulin006@iutbayonne.univ-pau.fr>
2024-08-20 12:29:00 +0200

    version 2.5 ajout include.php pour centraliser autoload et initialisation de TWIG

--- config/connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-try{
-    $pdo = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME , DB_USER, DB_PASS);
-}catch (PDOException $e){
-    echo "Erreur de connexion à la base de données : ".$e->getMessage();
-    die();
-}
--- include.php
+++ include.php
@@ -1,13 +1,7 @@
 <?php
-//ajout de l’autoload de composer
+
+//Ajout de l'autoload de composer
 require_once 'vendor/autoload.php';
 
-
-//Chargement de la page des constantes
-require_once 'config/constantes.php';
-
-//Chargement de la connexion à la base de données en pdo
-require_once 'config/connexion.php';
-
-//initialisation de twig
+//Ajout du code pour initialiser twig
 require_once 'config/twig.php';
