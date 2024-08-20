8e4e84411ed773ffb8fa69a9e1d2581a5d1c663c
Antoine Moulin <amoulin006@iutbayonne.univ-pau.fr>
2024-08-20 12:42:09 +0200

    version 2.6 centralisation connexion et prefixe des tables

--- include.php
+++ include.php
@@ -3,5 +3,11 @@
 //Ajout de l'autoload de composer
 require_once 'vendor/autoload.php';
 
+//Ajout du fichier constantes qui permet de configurer le site
+require_once 'config/constantes.php';
+
 //Ajout du code pour initialiser twig
 require_once 'config/twig.php';
+
+//Ajout du code pour initialiser la connexion à la base de données
+require_once 'config/connexion.php';
--- recette.php
+++ recette.php
@@ -1,31 +1,7 @@
 <?php
 
-//ajout de l’autoload de composer
-require_once 'vendor/autoload.php';
-
-//ajout de la classe IntlExtension et creation de l’alias IntlExtension
-use Twig\Extra\Intl\IntlExtension;
-
-//initialisation twig : chargement du dossier contenant les templates
-$loader = new Twig\Loader\FilesystemLoader('templates');
-
-//Paramétrage de l'environnement twig
-$twig = new Twig\Environment($loader, [
-    /*passe en mode debug à enlever en environnement de prod : permet d'utiliser dans un templates {{dump
-    (variable)}} pour afficher le contenu d'une variable. Nécessite l'utilisation de l'extension debug*/
-    'debug' => true,
-    // Il est possible de définir d'autre variable d'environnement
-    //...
-]);
-
-//Définition de la timezone pour que les filtres date tiennent compte du fuseau horaire français.
-$twig->getExtension(\Twig\Extension\CoreExtension::class)->setTimezone('Europe/Paris');
-
-//Ajouter l'extension debug
-$twig->addExtension(new \Twig\Extension\DebugExtension());
-
-//Ajout de l'extension d'internationalisation qui permet d'utiliser les filtres de date dans twig
-$twig->addExtension(new IntlExtension());
+//Ajout du code commun à toutes les pages
+require_once 'include.php';
 
 
 
@@ -36,13 +12,11 @@ $id_recette = isset($_GET['id_recette'])?$_GET['id_recette']:null;
 
 
 
-//Connexion à la base de données en pdo
-$pdo = new PDO('mysql:host=localhost;dbname=yabontiap_bd', 'root', '');
 
 //Construction de la requête
-$sql = "SELECT R.*, RI.*, I.nom as ingredient_nom FROM yabontiap_recette R
-INNER JOIN yabontiap_recette_ingredient RI ON R.id=RI.id_recette
-INNER JOIN yabontiap_ingredient I ON RI.id_ingredient=I.id 
+$sql = "SELECT R.*, RI.*, I.nom as ingredient_nom FROM " . PREFIXE_TABLE . "recette R
+INNER JOIN " . PREFIXE_TABLE . "recette_ingredient RI ON R.id=RI.id_recette
+INNER JOIN " . PREFIXE_TABLE . "ingredient I ON RI.id_ingredient=I.id 
 WHERE R.id=:id_recette";
 
 $pdoStatement = $pdo->prepare($sql);
