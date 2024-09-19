7cf1f432f599f2384f8b630c66a4586b389c7461
Antoine Moulin <amoulin006@iutbayonne.univ-pau.fr>
2024-09-19 17:27:54 +0200

    version 4.3 gestion de la page recettes par le controleur

--- controllers/controller_recette.class.php
+++ controllers/controller_recette.class.php
@@ -10,6 +10,27 @@ class ControllerRecette extends Controller{
     }
 
     public function lister(){
-        echo "lister recette";
+        
+        $idCategorie = isset($_GET['id_categorie']) ? $_GET['id_categorie'] : null;
+
+
+        
+
+        //Récupération des recettes
+        $managerRecette = new RecetteDao($this->getPdo());
+
+        if ($idCategorie == null) {
+            $recettes = $managerRecette->findAll();
+        } else {
+            $recettes = $managerRecette->findByCategorieWithDetail($idCategorie);
+        }
+
+        //Génère la vue
+        $template = $this->getTwig()->load('recettes.html.twig');
+                
+        echo $template->render(array(
+            'recettes' => $recettes, 
+            'menu' => 'recettes'
+        ));
     }
 }
